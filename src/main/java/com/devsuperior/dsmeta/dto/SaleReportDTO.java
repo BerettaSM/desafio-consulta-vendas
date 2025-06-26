22c8f2023b2d7c534be234b7b520bbbb788995f1
Ramon Saviato <ramonsaviato@hotmail.com>
2025-06-26 17:52:39 -0300

    add SaleReportProjection and SaleReportDTO

---
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleReportDTO.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dsmeta.dto;
+
+import java.time.LocalDate;
+
+import com.devsuperior.dsmeta.projections.SaleReportProjection;
+
+public class SaleReportDTO {
+    
+    private Long id;
+    private LocalDate date;
+    private Double amount;
+    private String sellerName;
+
+    public SaleReportDTO(Long id, LocalDate date, Double amount, String sellerName) {
+        this.id = id;
+        this.date = date;
+        this.amount = amount;
+        this.sellerName = sellerName;
+    }
+
+    public SaleReportDTO(SaleReportProjection projection) {
+        this(
+            projection.getId(),
+            projection.getDate(),
+            projection.getAmount(),
+            projection.getSellerName());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    @Override
+    public String toString() {
+        return "SaleReportDTO [id=" + id + ", date=" + date + ", amount=" + amount + ", sellerName=" + sellerName + "]";
+    }
+    
+}
